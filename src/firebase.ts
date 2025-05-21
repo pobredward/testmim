67bb984ba6215523eb02c59ee059f099493e92d2
pobredward <pobredward@gmail.com>
2025-05-21 22:09:29 +0900

    Initial commit

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3oi_xJHTcyTKoE38Vk6DHWp203SgVFOM",
-  authDomain: "test-zip-98f68.firebaseapp.com",
-  projectId: "test-zip-98f68",
-  storageBucket: "test-zip-98f68.firebasestorage.app",
-  messagingSenderId: "511331550799",
-  appId: "1:511331550799:web:21a63a1aeeb22edb404c3a",
-  measurementId: "G-L5Q5LJ8TY2"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
